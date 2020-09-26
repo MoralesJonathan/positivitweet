93b27c1d8393a4dc153dd0666b718a8c1e898913
Jonathan Morales <jonmorazav@yahoo.com>
2020-09-26 16:48:42 -0400

    add login page html

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const server = require('express')(),
+const express = require('express'),
+  server = express(),
   port = process.env.PORT || 8080,
   environment = server.get('env'),
+  path = require('path'),
   logger = require('morgan'),
   bodyParser = require('body-parser');
 
 server
+  .use(express.static(path.join(__dirname, 'public')))
   .use(bodyParser.json())
   .use(bodyParser.urlencoded({
     extended: true
@@ -16,6 +19,11 @@ server
   server.get('/', function(req, res) {
     res.send('Hello world!')
   })
+
+  server.get('/login', function(req, res) {
+    res.send('Hello world!')
+  })
+ 
 
   .listen(port, () => {
     console.log(`Server is running on port ${port} and is running with a ${environment} environment.`);
